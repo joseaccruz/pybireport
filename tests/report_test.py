ba526ba157e82f6ee6b30d779ca3e95f251bb298
JAC <joseaccruz@gmail.com>
2020-11-01 20:51:48 +0000

    WIP: this is still an unfinished version

--- tests/report_test.py
+++ tests/report_test.py
@@ -5,30 +5,29 @@ from pybireport.styles import DefaultStyle
 
 
 data = pd.DataFrame({
-	'a': [1, 2, 3],
-	'b': [4, 2, 3],
-	'c': [1, 'a', 3]})
+	'Produto': ['xx', 'a', 'yy'],
+	'Volume': [1, 2, 3],
+	'Qty': [4, 2, 3]})
 
 
 rep = Report("output/test_report.xlsx")
 
 p1 = rep.add_page(Page("sheet_1"))
-p2 = rep.add_page(Page("sheet_2"))
 
 vtitle = p1.add(Title("This is my first report")) \
 	.place_at(1, 1) \
 	.merge_cols(4)
 
-vtable = p1.add(Table("Count of stuff*", data)) \
+vtable = p1.add(Table("Contagens*", data)) \
 	.place_bellow(vtitle, rows=2)
 
 vlegend = p1.add(Legend("*Small legend at the bottom")) \
 	.place_bellow(vtable) \
-	.merge_cols(3)
+	.merge_cols(len(data.columns))
 
-vtitle2 = p1.add(Title("Other title")) \
-	.place_left(vtitle, cols=4)
+p2 = rep.add_page(Page("sheet_2"))
 
+p2.add(vtable)
 
 
 rep.generate()
